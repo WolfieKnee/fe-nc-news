381bb0148e1c169ab2338458112071a9cc468da2
WolfieKnee <david@davidwolfson.co.uk>
2024-02-06 14:51:18 +0000

    this will implement context for hard-coded user, and adds to header

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import { Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
 
 import Header from "./components/Header"
 import Navigation from './components/Navigation'
 import ArticleList from './pages/ArticleList';
 import ViewArticle from './pages/ViewArticle';
+import UserContext from "./contexts/UserContext";
 import "./App.css";
 
 function App() {
+  // note: hard coded user
+  const [loggedInUser, setLoggedInUser] = useState("weegembump");
   return (
-    <>
+    <UserContext.Provider value = {{loggedInUser, setLoggedInUser}}>
       <Header/>
       <Navigation />      
       <main>
@@ -17,7 +21,7 @@ function App() {
           <Route path="/:article_id" element={<ViewArticle/>} />
         </Routes>
       </main>
-    </>
+    </UserContext.Provider>
   )
 }
 

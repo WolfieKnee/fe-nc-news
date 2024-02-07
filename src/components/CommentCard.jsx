67c68d85cf33a5b5688df67eac1909bb58c4cea7
WolfieKnee <david@davidwolfson.co.uk>
2024-02-07 20:51:39 +0000

    this adds working comment deleting

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -2,11 +2,7 @@ import { useContext } from "react";
 import UserContext from "../contexts/UserContext";
 import DeleteComment from "./DeleteComment";
 
-export default function CommentCard({
-	comment,
-	commentsList,
-	setCommentsList,
-}) {
+export default function CommentCard({ comment, setCommentsList }) {
 	const { loggedInUser } = useContext(UserContext);
 
 	const commentDate = new Date(comment.created_at);
@@ -21,12 +17,9 @@ export default function CommentCard({
 				On {dateStr}, {comment.author} commented:
 			</h3>
 			<p>"{comment.body}"</p>
-			<p>This comment has {comment.votes} votes.</p>
-			<p>#{comment.comment_id}</p>
 			{comment.author === loggedInUser ? (
 				<DeleteComment
 					comment_id={comment.comment_id}
-					commentsList={commentsList}
 					setCommentsList={setCommentsList}
 				/>
 			) : null}
--- src/components/DeleteComment.jsx
+++ src/components/DeleteComment.jsx
@@ -1,22 +1,25 @@
 import { useEffect, useState } from "react";
 import { newsAPIDelete } from "../utils/utils";
 
-export default function DeleteComment({
-	comment_id,
-	commentsList,
-	setCommentsList,
-}) {
+export default function DeleteComment({ comment_id, setCommentsList }) {
 	const [deleteRequest, setDeleteRequest] = useState(false);
+	const [isLoading, setIsLoading] = useState(false);
 	const [errorState, setErrorState] = useState(null);
-	const backupCommentList = [...commentsList];
 
 	useEffect(() => {
 		if (deleteRequest) {
-			browserDelete();
-			newsAPIDelete(`/comments/${comment_id}`).catch(() => {
-				setErrorState(true);
-				setCommentsList(backupCommentList);
-			});
+			setIsLoading(true);
+			setErrorState(false);
+			newsAPIDelete(`/comments/${comment_id}`)
+				.then(() => {
+					setIsLoading(false);
+					browserDelete();
+				})
+				.catch(() => {
+					setErrorState(true);
+					setIsLoading(false);
+					setDeleteRequest(false);
+				});
 		}
 	}, [deleteRequest]);
 
@@ -33,19 +36,17 @@ export default function DeleteComment({
 		});
 	};
 
-	const browserRestore = () => {
-		setCommentsList((tempCommentList) => {
-			return tempCommentList.filter(
-				(comment) => comment.comment_id !== comment_id
-			);
-		});
-	};
-
-	if (errorState) {
-		return (
-			<p>there was a problem deleting that comment, please try again</p>
-		);
-	}
-
-	return <button onClick={handleClick}>delete your comment</button>;
+	return (
+		<span>
+			<button onClick={handleClick} disabled={isLoading}>
+				delete your comment
+			</button>
+			{errorState ? (
+				<p>
+					there was a problem deleting that comment, please refresh
+					and try again
+				</p>
+			) : null}
+		</span>
+	);
 }
